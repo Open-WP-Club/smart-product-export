a3c59d352e6c78a1d96308f50686f6cd9663f3c7
Gabriel Kanev <contact@mrgkanev.eu>
2025-11-04 16:52:58 +0200

    Declare HPOS compatibility

--- smart-product-export.php
+++ smart-product-export.php
@@ -418,6 +418,15 @@ class Smart_Product_Export {
 }
 
 /**
+ * Declare HPOS compatibility
+ */
+add_action('before_woocommerce_init', function() {
+    if (class_exists(\Automattic\WooCommerce\Utilities\FeaturesUtil::class)) {
+        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility('custom_order_tables', __FILE__, true);
+    }
+});
+
+/**
  * Initialize the plugin
  */
 function spe_init() {
